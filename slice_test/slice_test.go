0f101c258752db4fc28d022f2117911b0124f0ce
golohas <985792686@qq.com>
2020-09-26 15:50:12 +0800

    slice_test.go

---
+++ slice_test/slice_test.go
@@ -0,0 +1,12 @@
+package slice_test
+
+import "testing"
+
+func TestSlice(t *testing.T) {
+	var sliceInt []int
+	var b bool
+	var s string
+	t.Log("int slice: ", sliceInt)
+	t.Log("bool b: ", b)
+	t.Log("string s: ", s)
+}
