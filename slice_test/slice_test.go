a02ac10233a4e6d8604a2a3eb31f316ddfad9e80
xjzhao <985792686@qq.com>
2020-09-27 21:42:23 +0800

    add slice_test

--- slice_test/slice_test.go
+++ slice_test/slice_test.go
@@ -10,3 +10,23 @@ func TestSlice(t *testing.T) {
 	t.Log("bool b: ", b)
 	t.Log("string s: ", s)
 }
+
+func TestMake(t *testing.T) {
+	var s = make([]int, 4, 8)
+	t.Log(s)
+	for i := 0; i < len(s); i++ {
+		s[i] = i + 2
+	}
+	t.Log(s)
+	s1 := s[2:3]
+	t.Log("s1 is: ", s1)
+	t.Logf("s1 len: %d, s1 cap: %d", len(s1), cap(s1))
+}
+
+func TestAppend(t *testing.T) {
+	s := []int{1, 2, 3, 4}
+	t.Logf("s'address :%p", &s)
+	s = append(s, 5)
+	t.Log(s)
+	t.Logf("s'address :%p", &s)
+}
