e577cf717bdb804975527fe17accf22ca5b52008
golohas <985792686@qq.com>
2020-10-02 23:02:28 +0800

    slice_test.go modify

--- slice_test/slice_test.go
+++ slice_test/slice_test.go
@@ -10,3 +10,9 @@ func TestSlice(t *testing.T) {
 	t.Log("bool b: ", b)
 	t.Log("string s: ", s)
 }
+
+func TestMake(t *testing.T) {
+	var s = make([]int, 4)
+	t.Log(s)
+	t.Logf("s len: %d, cap: %d", len(s), cap(s))
+}
